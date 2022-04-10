2bba2a0d4e02c6444a5f241c68b163b9027cf2fc
Hudson-Cole <hudson.cole0128@gtcc.edu>
2022-04-10 16:38:57 -0400

    commit

---
+++ src/strandHudson.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList
+
+
+public class strandHudson {
+
+    //variable initialized
+    private ArrayList <lightHudson> strand = new ArrayList<lightHudson>();
+
+    public strandHudson()
+}
